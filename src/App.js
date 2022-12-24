5d36b5131d7fa553bbe9d9c01cc5fb6fddd87b5a
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-12-24 20:06:42 +0200

    add the data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import Category from "./components/Category";
 import Header from "./components/Header";
 import ItemsList from "./components/ItemsList";
 import MyNavbar from "./components/Navbar";
+import { items } from "./data";
 
 function App() {
+  const [itemsData, setitemsData] = useState(items);
   return (
     <div className="App">
       <MyNavbar />
       <Container>
         <Header />
         <Category />
-        <ItemsList />
-        <ItemsList />
-        <ItemsList />
-        <ItemsList />
-        <ItemsList />
+        
+          <ItemsList items={itemsData} />
       </Container>
     </div>
   );
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,34 +1,36 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 
-function ItemsList() {
+function ItemsList({ items }) {
   return (
-    <Row className="my-3">
-      <Col sm="12">
-        <Card className="d-flex flex-row" style={{ borderRadius: "9px" }}>
-          <Card.Img
-            style={{
-              width: "230px",
-              height: "170px",
-              borderRadius: "8px",
-              margin: "1px",
-            }}
-            variant="top"
-            src="img1.jpg"
-          />
-          <Card.Body>
-            <Card.Title className="d-flex justify-content-between">
-              <div className="text-warning">Breakfast meal</div>
-              <div className="text-warning mx-3">50$</div>
-            </Card.Title>
-            <Card.Text>
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
+    <Row>
+      {items.length > 0 ? (
+        items.map((item) => (
+          <Col sm="12" className="my-1" key={item.id}>
+            <Card className="d-flex flex-row" style={{ borderRadius: "9px" }}>
+              <Card.Img
+                style={{
+                  width: "230px",
+                  height: "170px",
+                  borderRadius: "8px",
+                  margin: "1px",
+                }}
+                variant="top"
+                src={item.img}
+              />
+              <Card.Body>
+                <Card.Title className="d-flex justify-content-between">
+                  <div className="text-warning">{item.title}</div>
+                  <div className="text-warning mx-3">{item.price}</div>
+                </Card.Title>
+                <Card.Text>{item.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))
+      ) : (
+        <h2 className="text-center my-2">there is no data!!</h2>
+      )}
     </Row>
   );
 }
