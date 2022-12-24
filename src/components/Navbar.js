4488b63db34c7b266df242db16b0f7a2da01e886
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-12-24 13:47:32 +0200

    created the design of the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function MyNavbar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">
+          <div style={{ color: "#198754" }}>New Restaurant</div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          ></Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="text"
+              placeholder="Search..."
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
