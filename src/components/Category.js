63c06560f53de3092c56175c90a74802c7066289
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-12-25 16:48:00 +0200

    add some animation

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
+import { Bounce } from "react-reveal";
 
 function Category({ filterByCategory, Categories }) {
   return (
     <Row className="d-flex justify-content-evenly mb-5">
       <Col className="d-flex justify-content-center" sm="12" md="8">
-        <div>
-          {Categories.map((cat) => (
-            <Button
-              variant="outline-warning m-2"
-              onClick={(_) => filterByCategory(cat)}
-              key={cat}
-            >
-              {cat}
-            </Button>
-          ))}
-        </div>
+        <Bounce top cascade>
+          <div>
+            {Categories.map((cat) => (
+              <Button
+                variant="outline-warning m-2"
+                onClick={(_) => filterByCategory(cat)}
+                key={cat}
+              >
+                {cat}
+              </Button>
+            ))}
+          </div>
+        </Bounce>
       </Col>
     </Row>
   );
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,36 +1,39 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
+import { Zoom } from "react-reveal";
 
 function ItemsList({ items }) {
   return (
     <Row>
-      {items.length > 0 ? (
-        items.map((item) => (
-          <Col sm="12" className="my-1" key={item.id}>
-            <Card className="d-flex flex-row" style={{ borderRadius: "9px" }}>
-              <Card.Img
-                style={{
-                  width: "230px",
-                  height: "170px",
-                  borderRadius: "8px",
-                  margin: "1px",
-                }}
-                variant="top"
-                src={item.img}
-              />
-              <Card.Body>
-                <Card.Title className="d-flex justify-content-between">
-                  <div className="text-warning">{item.title}</div>
-                  <div className="text-warning mx-3">{item.price}</div>
-                </Card.Title>
-                <Card.Text>{item.description}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))
-      ) : (
-        <h2 className="text-center my-2">there is no data!!</h2>
-      )}
+      <Zoom right cascade>
+        {items.length > 0 ? (
+          items.map((item) => (
+            <Col sm="12" className="my-1" key={item.id}>
+              <Card className="d-flex flex-row" style={{ borderRadius: "9px" }}>
+                <Card.Img
+                  style={{
+                    width: "230px",
+                    height: "170px",
+                    borderRadius: "8px",
+                    margin: "1px",
+                  }}
+                  variant="top"
+                  src={item.img}
+                />
+                <Card.Body>
+                  <Card.Title className="d-flex justify-content-between">
+                    <div className="text-warning">{item.title}</div>
+                    <div className="text-warning mx-3">{item.price}</div>
+                  </Card.Title>
+                  <Card.Text>{item.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <h2 className="text-center my-2">there is no data!!</h2>
+        )}
+      </Zoom>
     </Row>
   );
 }
